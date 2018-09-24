c8387308526b20506205b4b97225d0b4400b893d
Bret O'Brien <bobrien@umich.edu>
2018-09-24 15:46:32 -0500

    fen functions to output board state

--- cpp/chess/include/fen.h
+++ cpp/chess/include/fen.h
@@ -1,63 +1,84 @@
 #pragma once
 
 #include "squares.h"
-#include "pieces.h"
-#include "board.h"
+#include <string>
 
-struct board_state
-std::string fen(const board_state& brd) {
+namespace fen {
+
+bool whiteToMove(int clock) {
+	return clock % 2 == 0;
+}
+
+template<typename T>
+std::string to_string(const T& squares) {
 	std::string fen;
     int empties = 0;
 
-    for (int i = 0; i < NUM_SQUARES; i++) {
-        if (brd[i]) {
-            if (empties) {
-                fen += std::to_string(empties);
-                empties = 0;
-            }
-            fen += brd[i];
-        }
-        else {
-            empties++;
-        }
-        
-        if (file(i) == 'h') {
-            if (empties) {
-                fen += std::to_string(empties);
-                empties = 0;
-            }
-            fen += '/';
-        }
-    }
-    fen.pop_back(); //delete ending slash
-    
-    fen += ' ';
-	fen += brd.whiteToMove() ? 'w' : 'b';
-    fen += ' ';
-    
-    if (!brd.whiteCastleShort() &&
-		!brd.whiteCastleLong &&
-		!brd.blackCastleShort() &&
-		!brd.blackCastleLong()) { fen += '-'; }
-	else {
-		if (brd.whiteCastleShort())
-			fen += White::KING;
-		if (brd.whiteCastleLong)
-			fen += White::QUEEN;
-		if (brd.blackCastleShort())
-			fen += Black::KING;
-		if (brd.blackCastleLong())
-			fen += Black::QUEEN;
+	for (int row = BOARD_WIDTH - 1; row >= 0; row--) {
+		for (int col = 0; col < BOARD_WIDTH; col++) {
+			char piece = squares[row * BOARD_WIDTH + col];
+			if (piece) {
+				if (empties) {
+					fen += std::to_string(empties);
+					empties = 0;
+				}
+				fen += piece;
+			}
+			else
+				empties++;
+		}
+		if (empties) {
+			fen += std::to_string(empties);
+		}
+		fen += '/';
+		empties = 0;
 	}
-		
+    fen.pop_back(); //removes last slash
+	return fen;
+}
+
+std::string to_string(const CastleFlags flags) {
+	std::string fen;
+    if (!flags.whiteCastleShort &&
+		!flags.whiteCastleLong &&
+		!flags.blackCastleShort &&
+		!flags.blackCastleLong)
+	{
+		return "-";
+	}
+	else {
+		if (flags.whiteCastleShort)
+			fen += 'K';
+		if (flags.whiteCastleLong)
+			fen += 'Q';
+		if (flags.blackCastleShort)
+			fen += 'k';
+		if (flags.blackCastleLong)
+			fen += 'q';
+		return fen;
+	}
+}
+
+template<typename T>
+std::string to_string(const T& squares, const CastleFlags flags, int clock, int en_passant = -1) {
+	std::string fen;
+	fen += to_string(squares);
     fen += ' ';
-    if (brd.enPassant() < 0)
-        fen += '-';
-    else {
-        fen += file(brd.enPassant);
-        fen += rank(brd.enPassant);
-    }
+	fen += whiteToMove(clock) ? 'w' : 'b';
+    fen += ' ';
+	fen += to_string(flags);
+    fen += ' ';
+	fen += squares::to_string(en_passant);
     return fen;
 }
 
-board_state init(std::string fen);
+int full_clock(int clock) {
+	return (clock + 1) / 2;
+}
+
+std::string prefix(int clock) { 
+	std::string dot = whiteToMove(clock) ? ". " : "...";
+	return std::to_string(full_clock(clock)) + dot; 
+}
+
+}
