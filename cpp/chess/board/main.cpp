5f3d7b1d1caab859ad77a1d926c5a5eade9ad7be
Bret O'Brien <bobrien@umich.edu>
2018-09-24 15:47:49 -0500

    ignore whitespace and exit on error

--- cpp/chess/board/main.cpp
+++ cpp/chess/board/main.cpp
@@ -1,14 +1,28 @@
 
 #include "chess/include/board.h"
+#include "chess/include/fen.h"
+#include "include/read.h"
 #include <iostream>
 #include <string>
-
 
 using std::string;
 using std::cin;
 using std::cout;
+using std::cerr;
 using std::endl;
 
 int main(int argc, char* argv[]) {
 	Board brd;
+	string move;
+	while (getline(cin, move)) {
+		move = getword(move);
+		if (move.empty())
+			continue;
+		if (brd.TryMove(move))
+			cout << fen::to_string(brd, brd.flags(), brd.clock(), brd.enPassant()) << endl;
+		else {
+			cerr << "ERROR: move=" << move << " failed" << endl;
+			return 1;
+		}
+	}
 }
