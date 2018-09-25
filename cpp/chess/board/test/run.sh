060957f69d2370e607cd6c6f6331e5e06999ff7a
Bret O'Brien <bobrien@umich.edu>
2018-09-25 15:52:39 -0500

    init arg support

--- cpp/chess/board/test/run.sh
+++ cpp/chess/board/test/run.sh
@@ -14,10 +14,16 @@ trap "restore" EXIT
 failures=0
 
 for test in $(ls cases); do
+	if [ -f cases/$test/init ]; then
+		init=$(cat cases/$test/init)
+	else
+		init=''
+	fi
+
 	printf "$green"
 	echo [$test]
 	printf "$red"
-	if ! diff cases/$test/output <(cut -d# -f1 <cases/$test/input | brd); then
+	if ! diff cases/$test/output <(cut -d# -f1 <cases/$test/input | brd "$init"); then
 		((failures++))
 	fi
 done
