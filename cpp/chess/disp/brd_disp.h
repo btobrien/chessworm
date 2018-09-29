75cd2eb34060c17ae90b7b8006d1873f8fcabac1
Bret O'Brien <bobrien@umich.edu>
2018-09-29 09:10:47 -0500

    temp border added -- todo: make separate decorator

--- cpp/chess/disp/brd_disp.h
+++ cpp/chess/disp/brd_disp.h
@@ -13,7 +13,13 @@ inline bool isRowDelim(char c) {
 void DisplayFen(const std::string& fen) {
 	int i = 0;
 	bool isLight = true;
+	std::cout << "\u250C";
+	for (int col = 0; col < (2 * BOARD_WIDTH) + 1; col++)
+		std::cout << "\u2500";
+	std::cout << "\u2510\n";
+	
 	for (int row = 0; row < BOARD_WIDTH; row++) {
+		std::cout << "\u2502";
 		char c;
 		c = fen[i++];
 		while(!isRowDelim(c)) {
@@ -30,8 +36,14 @@ void DisplayFen(const std::string& fen) {
 			}
 			c = fen[i++];
 		}
+		std::cout << ' ';
+		std::cout << "\u2502";
 		std::cout << '\n';
 	}
+	std::cout << "\u2514";
+	for (int col = 0; col < (2 * BOARD_WIDTH) + 1; col++)
+		std::cout << "\u2500";
+	std::cout << "\u2518" << std::endl;
 }
 
 int fullClock(int clock) {
