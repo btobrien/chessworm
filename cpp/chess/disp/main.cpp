d64b96dc1ad6f2cf9d8fe729af714701f771733b
Bret O'Brien <bobrien@umich.edu>
2018-09-29 10:31:01 -0500

    consider lead char=- ad nullstring

--- cpp/chess/disp/main.cpp
+++ cpp/chess/disp/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv) {
 
 	std::string fen;
 	getline(std::cin, fen);
-	if (fen.empty() || fen == "-")
+	if (fen.empty() || fen[0] == '-')
 		fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -";
 	DisplayFen(fen);
 
