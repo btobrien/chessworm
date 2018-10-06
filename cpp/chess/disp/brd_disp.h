3e8372a547a207646138ae864741a51b359f1f6b
Bret O'Brien <bobrien@umich.edu>
2018-10-05 21:21:29 -0500

    border plus sh utils

--- cpp/chess/disp/brd_disp.h
+++ cpp/chess/disp/brd_disp.h
@@ -17,7 +17,6 @@ inline bool isRowDelim(char c) {
 void DisplayFen(const std::string& fen) {
 	int i = 0;
 	for (int row = 0; row < BOARD_WIDTH; row++) {
-		std::cout << "\u2502";
 		char c;
 		c = fen[i++];
 		while(!isRowDelim(c)) {
@@ -31,11 +30,6 @@ void DisplayFen(const std::string& fen) {
 			c = fen[i++];
 		}
 		std::cout << ' ';
-		std::cout << "\u2502";
 		std::cout << '\n';
 	}
-	std::cout << "\u2514";
-	for (int col = 0; col < (2 * BOARD_WIDTH) + 1; col++)
-		std::cout << "\u2500";
-	std::cout << "\u2518" << std::endl;
 }
