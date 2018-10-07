54510358f5e2ce6edde00a47a10d6959aa2e9c74
Bret O'Brien <bobrien@umich.edu>
2018-10-07 18:09:01 -0500

    modified center to support leading spaces

--- sh/cntr.sh
+++ sh/cntr.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+IFS='`'
 read line
 len=${#line}
 mid=$((($(tput cols)-$len)/2))
 pad=%"$mid"s
 printf "$pad"
-echo $line
+echo "$line"
 while read line; do
 	printf "$pad"
-	echo $line
+	echo "$line"
 done
