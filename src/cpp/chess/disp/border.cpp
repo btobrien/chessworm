8285e4bf13fbe260ff66479b1c5565610a6a1590
Bret O'Brien <bobrien@umich.edu>
2018-10-24 23:01:09 -0500

    cl arg for variable border size

--- src/cpp/chess/disp/border.cpp
+++ src/cpp/chess/disp/border.cpp
@@ -4,8 +4,11 @@
 const int BOARD_WIDTH = 8;
 
 int main(int argc, char** argv) {
+	int width = (2 * BOARD_WIDTH) + 1;
+	if  (argc > 1)
+		width = std::stoi(argv[1]);
 	std::cout << "\u250C";
-	for (int col = 0; col < (2 * BOARD_WIDTH) + 1; col++)
+	for (int col = 0; col < width; col++)
 		std::cout << "\u2500";
 	std::cout << "\u2510" << std::endl;
 	std::string line;
@@ -15,7 +18,7 @@ int main(int argc, char** argv) {
 		std::cout << "\u2502" << std::endl;
 	}
 	std::cout << "\u2514";
-	for (int col = 0; col < (2 * BOARD_WIDTH) + 1; col++)
+	for (int col = 0; col < width; col++)
 		std::cout << "\u2500";
 	std::cout << "\u2518" << std::endl;
 	return 0;
