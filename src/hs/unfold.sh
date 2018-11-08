5001b52f0152c74312ee5783d2a968270212ada3
Bret O'Brien <bobrien@umich.edu>
2018-11-07 21:54:37 -0600

    removed sed parsing of 2d arrays

--- src/hs/unfold.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# note: only supports empty prefix tree (i.e. [[]])
-sed 's/\[\["//' | sed 's/"\]\]//' | sed 's/\[\[//' | sed 's/\]\]//' | sed 's/"\],\["/\]/g' | sed 's/\],\[/\]/g' | sed 's/","/ /g' | tr ']' '\n'
