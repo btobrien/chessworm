16ce4707c3f3b7cb74c3c757c9cdcb61e0618285
Bret O'Brien <bobrien@umich.edu>
2019-01-27 04:14:29 -0600

    move chess battle to chess board

--- src/hs/Chess/Battle.hs
+++
@@ -1,8 +0,0 @@
-
-module Chess.Battle where
-
-import qualified Chess.Sides as Side
-import qualified Chess.Pieces as Piece
-import qualified Chess.Squares as Square
-import qualified Chess.Castle as Castle
-
