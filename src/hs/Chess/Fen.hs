7141439038d5e40512ebc82bb6f23789dbb14e1f
Bret O'Brien <bobrien@umich.edu>
2019-02-24 20:02:42 -0600

    implemented Fen of Soldiers

--- src/hs/Chess/Fen.hs
+++ src/hs/Chess/Fen.hs
@@ -4,11 +4,36 @@ module Chess.Fen where
 import Chess.Color
 import Chess.Soldier
 import Utils
+import Chess.Squares
+import Chess.Pieces
+import Data.List hiding (sortOn)
 
 toFen :: [Colored Soldier] -> String
-toFen = sortedToFen . sortOn (location . val)
+toFen = 
+    intercalate "/" . 
+    map fenOfRank . 
+    reverse .
+    groupRanks . 
+    sortOn (location . val)
 
-sortedToFen = const "test"
+groupRanks :: [Colored Soldier] -> [[Colored Soldier]]
+groupRanks soldiers = map rankFilter ranks
+    where 
+    rankFilter rank = filter ((rank==) . rankOf . location . val) soldiers
 
-fenPiece :: (Colored Soldier, Colored Soldier) -> String
-fenPiece = undefined
+fenOfRank :: [Colored Soldier] -> String
+fenOfRank [] = show dimension
+fenOfRank soldiers = pre ++ (concatMap fenOfPair $ buddies soldiers) ++ post
+    where
+    pre = showSpaces . fromEnum . fileOf . location . val . head $ soldiers
+    post = showPiece . last $ soldiers
+
+fenOfPair :: (Colored Soldier, Colored Soldier) -> String
+fenOfPair (x,y) = showPiece x ++ showSpaces (spacesBetween x y)
+    where spacesBetween x y = subtract 1 $ diffOn (location . val) x y
+
+showSpaces :: Int -> String
+showSpaces diff = if diff /= 0 then show diff else "" 
+
+showPiece :: Colored Soldier -> String
+showPiece soldier = show . fmap authority $ soldier
