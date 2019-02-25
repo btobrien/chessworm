ee5d5bdd7e59c181802b4e0b5586b09c6c617d96
Bret O'Brien <bobrien@umich.edu>
2019-02-24 20:01:56 -0600

    reversed order of ranks in squares and utilized everyone enum function

--- src/hs/Chess/Squares.hs
+++ src/hs/Chess/Squares.hs
@@ -4,27 +4,33 @@ import Utils
 import Control.Applicative
 
 data Square =
-    A8 | B8 | C8 | D8 | E8 | F8 | G8 | H8 |
-    A7 | B7 | C7 | D7 | E7 | F7 | G7 | H7 |
-    A6 | B6 | C6 | D6 | E6 | F6 | G6 | H6 |
-    A5 | B5 | C5 | D5 | E5 | F5 | G5 | H5 |
-    A4 | B4 | C4 | D4 | E4 | F4 | G4 | H4 |
-    A3 | B3 | C3 | D3 | E3 | F3 | G3 | H3 |
+    A1 | B1 | C1 | D1 | E1 | F1 | G1 | H1 |
     A2 | B2 | C2 | D2 | E2 | F2 | G2 | H2 |
-    A1 | B1 | C1 | D1 | E1 | F1 | G1 | H1
+    A3 | B3 | C3 | D3 | E3 | F3 | G3 | H3 |
+    A4 | B4 | C4 | D4 | E4 | F4 | G4 | H4 |
+    A5 | B5 | C5 | D5 | E5 | F5 | G5 | H5 |
+    A6 | B6 | C6 | D6 | E6 | F6 | G6 | H6 |
+    A7 | B7 | C7 | D7 | E7 | F7 | G7 | H7 |
+    A8 | B8 | C8 | D8 | E8 | F8 | G8 | H8
         deriving (Enum, Eq, Ord)
 
 dimension :: Int
 dimension = 8
-
-squares :: [Square]
-squares = enumFrom . toEnum $ 0
 
 data File = A | B | C | D | E | F | G | H
     deriving (Show, Enum, Eq)
 
 data Rank = R1 | R2 | R3 | R4 | R5 | R6 | R7 | R8
     deriving (Show, Enum, Eq)
+
+squares :: [Square]
+squares = everyone
+
+files :: [File]
+files = everyone
+
+ranks :: [Rank]
+ranks = everyone
 
 toFile :: Char -> Maybe File
 toFile 'a' = Just A
@@ -52,16 +58,13 @@ fileOf :: Square -> File
 fileOf = toEnum . (`mod` dimension) . fromEnum
 
 rankOf :: Square -> Rank
-rankOf = toEnum . rankFlip . (`div` dimension) . fromEnum
+rankOf = toEnum . (`div` dimension) . fromEnum
 
 toSquare :: (File,Rank) -> Square
 toSquare (f,r) = toEnum $ x + (dimension * y)
     where
     x = fromEnum f
-    y = rankFlip (fromEnum r)
-
-rankFlip :: Int -> Int
-rankFlip r = dimension - r - 1
+    y = fromEnum r
 
 tryRead :: String -> Maybe Square
 tryRead = undefined
