76dcc546ae00b3b7b80c99017222c7e9b9666088
Bret O'Brien <bobrien@umich.edu>
2019-12-21 17:11:34 -0600

    added make top support

--- app/brd/dump.sh
+++ app/brd/dump.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+disp=${BOARD:-board}
+
+if [ -f flip.bool ]; then
+    $disp <state.fen | flip
+else
+    $disp <state.fen
+fi
 
--- app/brd/install.sh
+++ app/brd/install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cd $(dirname $0)
 mkdir -p  ~/.config/brd/brds
-chmod +x *.sh
 cp *.sh ~/.config/brd/
 cp ./brd.sh ~/bin/brd
 
